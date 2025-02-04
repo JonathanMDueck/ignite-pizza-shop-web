55a49bc818b98e64ec060f9206635e546f5228a9
Jonathan Dueck <jonathan@dueck.com.br>
2025-02-03 22:44:07 -0300

    Adding tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import path from "node:path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+import { defineConfig, UserConfig } from "vite";
+import { InlineConfig } from "vitest";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,4 +11,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  test: {
+    globals: true,
+    setupFiles: ["./test/setup.ts"],
+    environment: "happy-dom",
+  },
+} as UserConfig & {
+  test: InlineConfig;
+});
